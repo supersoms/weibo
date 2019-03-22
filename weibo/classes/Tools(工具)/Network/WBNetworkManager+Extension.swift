4e73fb9fce7477a02beb3f1cedcd8e741ea72189
麒麟 <qilin@qilindeiMac.local>
2019-03-22 21:27:46 +0800

    实现下拉刷新

--- weibo/classes/Tools(工具)/Network/WBNetworkManager+Extension.swift
+++ weibo/classes/Tools(工具)/Network/WBNetworkManager+Extension.swift
@@ -4,11 +4,13 @@ import Foundation
 extension WBNetworkManager {
     
     /// 加载微博数据字典数组
+    /// parameter since_id: API接口声明，若指定此参数，则返回ID比since_id大的微博（即比since_id时间晚的微博），默认为0。
+    /// parameter max_id: API接口声明，若指定此参数，则返回ID小于或等于max_id的微博，默认为0。
     /// parameter completion: 完成回调[list: 微博字典数组/是否成功]
-    func statusList(completion: @escaping (_ list:[[String: Any]]?, _ isSuccess: Bool)->()){
+    func statusList(since_id: Int64 = 0, max_id: Int64 = 0, completion: @escaping (_ list:[[String: Any]]?, _ isSuccess: Bool)->()){
         print("===开始加载数据===")
         let url = "https://api.weibo.com/2/statuses/home_timeline.json"
-        let params = [String: Any]()
+        let params = ["since_id":since_id,"max_id":max_id] //此两个参数是下拉刷新微博时用到的参数
         
         WBNetworkManager.shared.tokenRequest(url: url, params: params) { (json, isSuccess) in
             //从json中获取 statuses 字典数组,如果 as? 失败,那result == nil
--- weibo/classes/Tools(工具)/Network/WBStatusListViewModel.swift
+++ weibo/classes/Tools(工具)/Network/WBStatusListViewModel.swift
@@ -14,7 +14,10 @@ class WBStatusListViewModel {
     //当微博数据通过模型转换为数组之后，需要返回给调用者，那此时就要用到闭包
     func loadStatus(completion: @escaping (_ success: Bool)->()){
         
-        WBNetworkManager.shared.statusList { (list, isSuccess) in
+        //since_id 取出数组中第一条微博的id
+        let since_id = statusList.first?.id ?? 0 //因first后面带了?号，有可能没有这个id,所以再加个??取0的值
+        
+        WBNetworkManager.shared.statusList(since_id: since_id, max_id: 0) { (list, isSuccess) in
             
             //1: 字典(map)转模型,有可能list为nil没数据，所以用??处理，然后通过 as? [WBStatus] 将array转为[WBStatus]
             guard let array = NSArray.yy_modelArray(with: WBStatus.self, json: list ?? []) as? [WBStatus] else {
@@ -23,8 +26,11 @@ class WBStatusListViewModel {
                 return
             }
             
+            print("刷新了\(array.count)数据")
+            
             //2: 拼接数据
-            self.statusList += array
+            //>2.1 下拉刷新时，应该将结果数组放在数组最前面
+            self.statusList = array + self.statusList
             
             //3: 调用闭包告诉调用者成功
             completion(isSuccess)
