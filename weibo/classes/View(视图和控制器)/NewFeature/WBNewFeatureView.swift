d1384da4aa7a0112749d6489b7123f20e12f2f6b
麒麟 <qilin@qilindeiMac.local>
2019-03-24 21:43:01 +0800

    准备欢迎视图与新特性视图

---
+++ weibo/classes/View(视图和控制器)/NewFeature/WBNewFeatureView.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//新特性视图
+class WBNewFeature: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.orange
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ weibo/classes/View(视图和控制器)/NewFeature/WBWecomeView.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//APP欢迎页视图
+class WBWecomeView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
