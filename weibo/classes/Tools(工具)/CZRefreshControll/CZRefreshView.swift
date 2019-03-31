49644d670528388e4c7b41a9b401cc3bd9fc8bfc
麒麟 <qilin@qilindeiMac.local>
2019-03-31 15:57:53 +0800

    旋转的动画处理

--- weibo/classes/Tools(工具)/CZRefreshControll/CZRefreshView.swift
+++ weibo/classes/Tools(工具)/CZRefreshControll/CZRefreshView.swift
@@ -3,13 +3,27 @@ import UIKit
 /// 刷新视图 - 负者刷新相关的UI显示和动画
 class CZRefreshView: UIView {
     
+    /***
+     iOS系统中，UIView封装的旋转动画
+     - 默认是顺时针旋转
+     - 就近原则
+     - 要想实现同方向旋转，需要调整一个非常小的数字(近)
+     - 如果想实现360度旋转，需要核心动画CABaseAnimation
+     **/
     var refreshState: CZRefreshState = .Normal { //刷新状态
         didSet {
             switch refreshState {
             case .Normal:
                 tipLabel.text = "继续使劲拉..."
+                UIView.animate(withDuration: 0.25) {
+                   self.tipIcon.transform = CGAffineTransform.identity //恢复成初始状态
+                }
             case .Pulling:
                 tipLabel.text = "放手就刷新..."
+                UIView.animate(withDuration: 0.25) {
+                    //pi是180度旋转，- 0.001是为了解决顺时针旋转，实现同方向旋转
+                    self.tipIcon.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI - 0.001))
+                }
             case .WillRefresh:
                 tipLabel.text = "正在刷新..."
             }
